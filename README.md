65885225d6113f98b6321c910b78719d8e76a102
lagartaSadia <campolino.l@gmail.com>
2024-08-19 03:02:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# <a href="">Acesse aqui me currículo e portifólio</a>
---
+++ scritp.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("button")
+
+button.addEventListener("click", () => {
+  alert("Por enquanto não consigo enviar suas informações! Mas você pode acessar minha página no Github. 'u'")
+})
